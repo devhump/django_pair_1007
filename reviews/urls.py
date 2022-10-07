a5c272aa74b1525972b7aff3a02d35a7a29882e2
hae-on <solbi2004@naver.com>
2022-10-07 13:31:15 +0900

    디자인

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'reviews'
+app_name = "reviews"
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
-    path('create/', views.create, name='create'),
-    path('<int:pk>/', views.detail, name="detail"),
-    path('<int:pk>/update', views.update, name='update'),
-    path('<int:pk>/delete', views.delete, name='delete'),
+    path("", views.home, name="home"),
+    path("index/", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/update", views.update, name="update"),
+    path("<int:pk>/delete", views.delete, name="delete"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -4,15 +4,20 @@ from reviews.forms import ReviewsForm
 from .models import Reviews
 
 # Create your views here.
+def home(request):
+    return render(request, "reviews/home.html")
+
+
 def index(request):
 
     reviews = Reviews.objects.all()
 
     context = {
-        'reviews' : reviews,
+        "reviews": reviews,
     }
 
-    return render(request, 'reviews/index.html', context)
+    return render(request, "reviews/index.html", context)
+
 
 def create(request):
 
@@ -20,15 +25,15 @@ def create(request):
         reviews_form = ReviewsForm(request.POST)
         if reviews_form.is_valid():
             reviews_form.save()
-            return redirect('reviews:index')
+            return redirect("reviews:index")
     else:
         reviews_form = ReviewsForm()
 
     context = {
-        'reviews_form' : reviews_form,
+        "reviews_form": reviews_form,
     }
 
-    return render(request, 'reviews/create.html', context)
+    return render(request, "reviews/create.html", context)
 
 
 def detail(request, pk):
@@ -36,35 +41,35 @@ def detail(request, pk):
     review = Reviews.objects.get(pk=pk)
 
     context = {
-        'review' : review,
+        "review": review,
     }
 
-    return render(request, 'reviews/detail.html', context)
+    return render(request, "reviews/detail.html", context)
 
 
 def update(request, pk):
 
     review = Reviews.objects.get(pk=pk)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         reviews_form = ReviewsForm(request.POST, instance=review)
         if reviews_form.is_valid():
             reviews_form.save()
-            return redirect('reviews:detail', review.pk)
+            return redirect("reviews:detail", review.pk)
     else:
-        reviews_form  = ReviewsForm(instance=review)
+        reviews_form = ReviewsForm(instance=review)
 
     context = {
-        'reviews_form' : reviews_form,
+        "reviews_form": reviews_form,
     }
 
-    return render(request, 'reviews/update.html', context)
+    return render(request, "reviews/update.html", context)
 
 
-#get으로 구현, post로 변경 필요
+# get으로 구현, post로 변경 필요
 def delete(request, pk):
 
     review = Reviews.objects.get(pk=pk)
     review.delete()
 
-    return redirect('reviews:index')
+    return redirect("reviews:index")
